16c94e2da9826d45cf37d6987dcd276390dd15c1
wangsheng <wangsheng@gionee.com>
2018-01-23 11:48:11 +0800

    内粘性头部效果

--- MyApplicationDemo/app/src/main/java/com/example/ws/myapplicationdemo/MainActivity.java
+++ MyApplicationDemo/app/src/main/java/com/example/ws/myapplicationdemo/MainActivity.java
@@ -1,48 +1,113 @@
 package com.example.ws.myapplicationdemo;
 
-import android.nfc.Tag;
+import android.database.DataSetObserver;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "MainActivity";
+    public static final String TAG = "Main_Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Exception exception = new Exception("this is test");
-        exception.printStackTrace();
-        Log.d(TAG, "onCreate");
-    }
+        ListView listView = (ListView) findViewById(R.id.list_id);
+        listView.setAdapter(new ListAdapter() {
+            @Override
+            public boolean areAllItemsEnabled() {
+                return false;
+            }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+            @Override
+            public boolean isEnabled(int i) {
+                return true;
+            }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            @Override
+            public void registerDataSetObserver(DataSetObserver dataSetObserver) {
 
-        // noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+            }
 
-        return super.onOptionsItemSelected(item);
+            @Override
+            public void unregisterDataSetObserver(DataSetObserver dataSetObserver) {
+
+            }
+
+            @Override
+            public int getCount() {
+                return 20;
+            }
+
+            @Override
+            public Object getItem(int i) {
+                return i;
+            }
+
+            @Override
+            public long getItemId(int i) {
+                return 1;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return false;
+            }
+
+            @Override
+            public View getView(int i, View view, ViewGroup viewGroup) {
+                TextView textView = new TextView(MainActivity.this);
+                if (i < 5) {
+                    textView.setText("hello world  " + i);
+                } else {
+                    textView.setText("text " + i);
+                }
+
+                return textView;
+            }
+
+            @Override
+            public int getItemViewType(int i) {
+                return 0;
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return 1;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+        });
+
+        final ImageView imgView = (ImageView) findViewById(R.id.top_id);
+
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount,
+                    int totalItemCount) {
+                if (firstVisibleItem >= 4 && view.getChildAt(0) != null) {
+                    View firstView = view.getChildAt(0);
+                    int tralateY = -((firstVisibleItem - 4) * firstView.getMeasuredHeight())
+                            + firstView.getTop();
+                    imgView.setTranslationY(tralateY);
+                } else {
+                    imgView.setTranslationY(0);
+                }
+            }
+        });
     }
 }
