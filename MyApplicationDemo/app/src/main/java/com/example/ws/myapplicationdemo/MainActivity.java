ebd89854a6666aaebb9047a85d5a6830be131713
wangsheng <wangsheng@gionee.com>
2018-01-23 14:00:22 +0800

    类粘性头部效果规范化

--- MyApplicationDemo/app/src/main/java/com/example/ws/myapplicationdemo/MainActivity.java
+++ MyApplicationDemo/app/src/main/java/com/example/ws/myapplicationdemo/MainActivity.java
@@ -1,113 +1,38 @@
 package com.example.ws.myapplicationdemo;
 
+import android.content.Intent;
 import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.ws.myapplicationdemo.stickylike.StickyLikeActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     public static final String TAG = "Main_Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ListView listView = (ListView) findViewById(R.id.list_id);
-        listView.setAdapter(new ListAdapter() {
-            @Override
-            public boolean areAllItemsEnabled() {
-                return false;
-            }
+    }
 
-            @Override
-            public boolean isEnabled(int i) {
-                return true;
-            }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sticky_like_btn:
+                startActivity(new Intent(this, StickyLikeActivity.class));
+                break;
+            default:
+                break;
+        }
 
-            @Override
-            public void registerDataSetObserver(DataSetObserver dataSetObserver) {
-
-            }
-
-            @Override
-            public void unregisterDataSetObserver(DataSetObserver dataSetObserver) {
-
-            }
-
-            @Override
-            public int getCount() {
-                return 20;
-            }
-
-            @Override
-            public Object getItem(int i) {
-                return i;
-            }
-
-            @Override
-            public long getItemId(int i) {
-                return 1;
-            }
-
-            @Override
-            public boolean hasStableIds() {
-                return false;
-            }
-
-            @Override
-            public View getView(int i, View view, ViewGroup viewGroup) {
-                TextView textView = new TextView(MainActivity.this);
-                if (i < 5) {
-                    textView.setText("hello world  " + i);
-                } else {
-                    textView.setText("text " + i);
-                }
-
-                return textView;
-            }
-
-            @Override
-            public int getItemViewType(int i) {
-                return 0;
-            }
-
-            @Override
-            public int getViewTypeCount() {
-                return 1;
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-        });
-
-        final ImageView imgView = (ImageView) findViewById(R.id.top_id);
-
-        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(AbsListView view, int scrollState) {
-
-            }
-
-            @Override
-            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount,
-                    int totalItemCount) {
-                if (firstVisibleItem >= 4 && view.getChildAt(0) != null) {
-                    View firstView = view.getChildAt(0);
-                    int tralateY = -((firstVisibleItem - 4) * firstView.getMeasuredHeight())
-                            + firstView.getTop();
-                    imgView.setTranslationY(tralateY);
-                } else {
-                    imgView.setTranslationY(0);
-                }
-            }
-        });
     }
 }
---
+++ MyApplicationDemo/app/src/main/java/com/example/ws/myapplicationdemo/stickylike/StickyLikeActivity.java
@@ -0,0 +1,119 @@
+package com.example.ws.myapplicationdemo.stickylike;
+
+import android.app.Activity;
+import android.database.DataSetObserver;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.ws.myapplicationdemo.MainActivity;
+import com.example.ws.myapplicationdemo.R;
+
+/**
+ * Created by ws on 18-1-23.
+ */
+
+public class StickyLikeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sticky_like_layout);
+        ListView listView = (ListView) findViewById(R.id.list_id);
+        listView.setAdapter(new ListAdapter() {
+            @Override
+            public boolean areAllItemsEnabled() {
+                return false;
+            }
+
+            @Override
+            public boolean isEnabled(int i) {
+                return true;
+            }
+
+            @Override
+            public void registerDataSetObserver(DataSetObserver dataSetObserver) {
+
+            }
+
+            @Override
+            public void unregisterDataSetObserver(DataSetObserver dataSetObserver) {
+
+            }
+
+            @Override
+            public int getCount() {
+                return 20;
+            }
+
+            @Override
+            public Object getItem(int i) {
+                return i;
+            }
+
+            @Override
+            public long getItemId(int i) {
+                return 1;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return false;
+            }
+
+            @Override
+            public View getView(int i, View view, ViewGroup viewGroup) {
+                TextView textView = new TextView(StickyLikeActivity.this);
+                if (i < 5) {
+                    textView.setText("hello world  " + i);
+                } else {
+                    textView.setText("text " + i);
+                }
+
+                return textView;
+            }
+
+            @Override
+            public int getItemViewType(int i) {
+                return 0;
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return 1;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+        });
+
+        final ImageView imgView = (ImageView) findViewById(R.id.top_id);
+
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount,
+                    int totalItemCount) {
+                if (firstVisibleItem >= 4 && view.getChildAt(0) != null) {
+                    View firstView = view.getChildAt(0);
+                    int tralateY = -((firstVisibleItem - 4) * firstView.getMeasuredHeight())
+                            + firstView.getTop();
+                    imgView.setTranslationY(tralateY);
+                } else {
+                    imgView.setTranslationY(0);
+                }
+            }
+        });
+    }
+}
